d885383d2a6ee6dab02370d88eb128f06844b326
wan230114 <1170101471@qq.com>
2020-03-30 12:23:17 +0800

    Update 0.5.6; Fixed bug about input `\n`.

--- extension.js
+++ extension.js
@@ -43,6 +43,31 @@ function sendQueuedText(text, waitTime = 50) {
     waitsQueue.push(waitTime);
 }
 
+function judgeCMD(CMD) {
+    // 单行统一false
+    // 多行判断缩进, 并且末尾是否存在/n，有缩进且无/n返回true
+    let A = CMD.split('\n')
+    console.log(CMD)
+    console.log(A)
+    if (A.length > 1) {
+        text = A[A.length - 1]
+        console.log(text)
+        istop = RegExp(/^\s+/g).test(text)
+    } else {
+        istop = false
+    }
+    console.log(istop)
+    return istop
+}
+// test:
+// 单行统一false
+// judgeCMD('   abcadf')    // false
+// judgeCMD('   abcadf\n')    // false
+// 多行判断缩进并且末尾是否存在/n
+// judgeCMD('abc\n    adf')    // true
+// judgeCMD('abc\n    adf\n')    // false
+
+
 // let runtimes = 0
 function queueLoop() {
     // runtimes += 1
@@ -60,7 +85,7 @@ function queueLoop() {
         if (isrunning) {            
             if (textQueue.length === 0 && pythonTerminal !== null && pythonTerminal._queuedRequests.length === 0) {
                 // vscode.window.showInformationMessage('满足条件, 发送换行符中');
-                pythonTerminal.sendText('\n');
+                // pythonTerminal.sendText('\n');
                 // vscode.commands.executeCommand('workbench.action.focusActiveEditorGroup');
                 isrunning = false;
             };
@@ -92,9 +117,11 @@ function activate(context) {
             updateFilename(filename, configuration.get('runInCurrentDirectory'));
         }
         // get selection & get snippet
-        const selection = editor !== undefined ? editor.selection : undefined;
-        if (editor === undefined || selection === undefined || selection.isEmpty ||
-            configuration.get("isAutoInputLine")) {  //defalt: true 
+        const selection = (editor !== undefined) ? editor.selection : undefined;
+        const isSelection = (editor === undefined || selection === undefined || selection.isEmpty) ? false : true
+        const isAutoInputLine = configuration.get("isAutoInputLine");
+        // 非选中状态下，或选中状态下AutoInputLine开启，计算start, end
+        if (isSelection === false || isAutoInputLine) {  //defalt: true
             let startLine, endLine;
             if (editor.selection.isEmpty) {
                 startLine = editor.selection.active.line + 1
@@ -102,15 +129,31 @@ function activate(context) {
             } else {
                 startLine = editor.selection.start.line + 1;
                 endLine = editor.selection.end.line + 1;
-            }
-            const command = `\n%load -r ${startLine}-${endLine} ${filename}\n`;
-            sendQueuedText(command, 500);
-            sendQueuedText('\n');
-        } else {
-            const selectedText = editor.document.getText(selection);
-            const command = selectedText.toString();
-            sendQueuedText(command, 500);
+            };
+            command = `\n%load -r ${startLine}-${endLine} ${filename}\n`;
         };
+        // 开始输入
+        if (isSelection) {  // 选中状态下
+            const selectedText = editor.document.getText(selection);
+            const command_text = selectedText.toString();
+            // isAutoInputLine功能开启
+            // sendQueuedText(isAutoInputLine ?command:command_text, 500);
+            if (isAutoInputLine) {
+                sendQueuedText(command, 500);
+                sendQueuedText('\n');
+                // 选中的末尾没有\n，且有缩进
+                if (judgeCMD(command_text)) {
+                    console.log('发送缩进处理换行中')
+                    sendQueuedText('\n', 300);
+                };
+            } else {
+                sendQueuedText(command_text, 500);
+            };
+        } else {  // 非选中状态下, 直接发送command
+            sendQueuedText(command);
+            sendQueuedText('\n');
+        };
+        sendQueuedText('\n');
         pythonTerminal.show(configuration.get("focusActiveEditorGroup"));  //defalt: true
         queueLoop();
     });
@@ -126,6 +169,7 @@ function activate(context) {
             updateFilename(filename, configuration.get('runInCurrentDirectory'));
         }
         sendQueuedText(`\n%load ${filename}\n`, 500);
+        sendQueuedText('\n');
         sendQueuedText('\n');
         pythonTerminal.show(configuration.get("focusActiveEditorGroup"));
         queueLoop();
